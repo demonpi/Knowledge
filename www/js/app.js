011142c1da3fcdd34363cf2f7dbb077d5abfeec3
demonpi <demonmail@sina.com>
2016-03-13 08:42:55 +0800

    增加j s

--- www/js/app.js
+++ www/js/app.js
@@ -41,11 +41,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.videochat', {
+      url: '/videochat',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/videochat.html'
         }
       }
     })
